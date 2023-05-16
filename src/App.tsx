dccffd5842a67749de8ab6bc8cdacf02ff45c959
libjared <libjared@users.noreply.github.com>
2023-05-15 21:38:12 -0500

    Implement nav and intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
-import logo from "./logo.svg";
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App--contain-width">
+        <nav>
+          <span className="App--nav-group App--nav-logo">
+            <a href="#Home">chunky</a>
+          </span>
+          <span className="App--nav-group App--nav-pages">
+            <a href="#Features">Features</a>
+            <a href="#Apps">Apps</a>
+            <a href="#Pricing">Pricing</a>
+            <a href="#Help">Help</a>
+          </span>
+          <span className="App--nav-group App--nav-actions">
+            <a href="#Login" className="App--link-secondary">
+              Login
+            </a>
+            <a href="#GetStarted" className="App--link-primary">
+              Get Started
+            </a>
+          </span>
+        </nav>
       </header>
+      <main>
+        <section className="App--intro">
+          <aside>a</aside>
+          <div className="App--contain-width">
+            <div className="App--intro-column">
+              <h1>Give your modded Minecraft experience a silk touch</h1>
+              <div className="App--intro-interactive">
+                <a href="#UseForFree">Use Chunky for free</a>
+                <p>
+                  <small>(No credit card required)</small>
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
